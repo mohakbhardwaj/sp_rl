0c1b71e21ffaa99c786699ad3223d72697808b17
Mohak Bhardwaj <mohak.bhardwaj@gmail.com>
2019-05-06 11:28:54 -0400

    running new experiments

--- scripts/run_dagger_linear_2d.sh
+++ scripts/run_dagger_linear_2d.sh
@@ -7,11 +7,11 @@ valid_envs=('graphEnv2DValidation-v1' 'graphEnv2DValidation-v2' 'graphEnv2DValid
 
 
 base_folder='../../sp_rl_new_experiments/'
-folders=(${base_folder}'dataset_2d_1/dagger_linear_quad' ${base_folder}'dataset_2d_2/dagger_linear_quad' ${base_folder}'dataset_2d_3/dagger_linear_quad'\
-         ${base_folder}'dataset_2d_4/dagger_linear_quad' ${base_folder}'dataset_2d_5/dagger_linear_quad' ${base_folder}'dataset_2d_6/dagger_linear_quad'\
-         ${base_folder}'dataset_2d_7/dagger_linear_quad' ${base_folder}'dataset_2d_8/dagger_linear_quad')
+folders=(${base_folder}'dataset_2d_1/dagger_linear_mix' ${base_folder}'dataset_2d_2/dagger_linear_mix' ${base_folder}'dataset_2d_3/dagger_linear_mix'\
+         ${base_folder}'dataset_2d_4/dagger_linear_mix' ${base_folder}'dataset_2d_5/dagger_linear_mix' ${base_folder}'dataset_2d_6/dagger_linear_mix'\
+         ${base_folder}'dataset_2d_7/dagger_linear_mix' ${base_folder}'dataset_2d_8/dagger_linear_mix')
 
-run_idxs=(0)
+run_idxs=(1 2 3 4 5 6 7)
 
 
 printf "Changing directories"
@@ -20,17 +20,17 @@ cd ../examples
 echo `pwd`
 
 
-num_iters=2
+num_iters=10
 episodes_per_iter=100
 num_valid_episodes=100
 num_test_episodes=200
 
 model='linear'
 expert='length_oracle'
-beta0=0.6
+beta0=0.5
 alpha=0.001
 gamma=0.5
-batch_size=32
+batch_size=64
 epochs=2
 weight_decay=0.0001
 seed_val=0
@@ -42,5 +42,5 @@ for ((i=0;i<${#run_idxs[@]};++i)); do
   python2.7 example_dagger.py --env ${train_envs[idx]} --valid_env ${valid_envs[idx]} --folder ${folders[idx]} --num_iters ${num_iters}\
          --num_episodes_per_iter ${episodes_per_iter} --num_valid_episodes ${num_valid_episodes} --num_test_episodes ${num_test_episodes}\
          --model ${model} --expert ${expert} --beta0 ${beta0} --alpha ${alpha} --gamma ${gamma} --batch_size ${batch_size} --epochs ${epochs}\
-         --weight_decay ${weight_decay} --seed_val ${seed_val} --quad_ftrs  #--plot
+         --weight_decay ${weight_decay} --seed_val ${seed_val} #--quad_ftrs  #--plot
 done
